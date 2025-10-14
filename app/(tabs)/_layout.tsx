f2a4c98fbf09ef75f2adc9ac39946be5ebaa1225
Nicole Ying <yingjiaye1108@gmail.com>
2025-10-14 02:10:55 -0700

    Icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,45 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="medication" options={{ title: "Medication" }} />
-      <Tabs.Screen name="bloodPressure" options={{ title: "Blood Pressure" }} />
-      <Tabs.Screen name="therapy" options={{ title: "Therapy" }} />
-      <Tabs.Screen name="explore" options={{ title: "Explore" }} />
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="medication" 
+        options={{ 
+          title: "Medication",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="medical" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="bloodPressure" 
+        options={{ 
+          title: "Blood Pressure",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="therapy" 
+        options={{ 
+          title: "Therapy",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="fitness" size={size} color={color} />
+          ),
+        }} 
+      />
     </Tabs>
   );
 }
