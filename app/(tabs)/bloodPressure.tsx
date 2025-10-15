586741549b3bddc09089b08a8979f7f044840170
Nicole Ying <yingjiaye1108@gmail.com>
2025-10-14 22:09:58 -0700

    Header2

--- app/(tabs)/bloodPressure.tsx
+++ app/(tabs)/bloodPressure.tsx
@@ -3,29 +3,29 @@ import { Ionicons } from '@expo/vector-icons';
 import { Stack } from "expo-router";
 import { useEffect, useState } from "react";
 import {
-    Alert,
-    Animated,
-    Dimensions,
-    FlatList,
-    Modal,
-    Pressable,
-    ScrollView,
-    Text,
-    TextInput,
-    View
+  Alert,
+  Animated,
+  Dimensions,
+  FlatList,
+  Modal,
+  Pressable,
+  ScrollView,
+  Text,
+  TextInput,
+  View
 } from "react-native";
 import { Swipeable } from "react-native-gesture-handler";
 import type { BloodPressureReading, BloodPressureStats } from "../../types/bloodPressure";
 import {
-    getBloodPressureCategory,
-    getCategoryColor,
-    getCategoryLabel
+  getBloodPressureCategory,
+  getCategoryColor,
+  getCategoryLabel
 } from "../../types/bloodPressure";
 import {
-    addBloodPressureReading,
-    calculateBloodPressureStats,
-    deleteBloodPressureReading,
-    loadBloodPressureReadings
+  addBloodPressureReading,
+  calculateBloodPressureStats,
+  deleteBloodPressureReading,
+  loadBloodPressureReadings
 } from "../lib/bloodPressureStorage";
 
 function formatDate(date: Date): string {
